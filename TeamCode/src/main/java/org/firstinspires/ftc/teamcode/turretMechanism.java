3d8922c0fc58ef9663ba1ae17f251b9e0dd90a1a
Sam <samuelkhan323@gmail.com>
2026-09-26 15:00:25 -0500

    added the correct dependencies

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/turretMechanism.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/turretMechanism.java
@@ -1,65 +1,65 @@
-package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
-
-import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
-
-public class turretMechanism {
-    private DcMotorEx turret;
-    private double kP = 0.0001;
-    private double kD = 0.000;
-    private double goalX = 0;
-    private double lastError = 0;
-    private double angleTolerance = 0.2;
-    private final double MAX_POWER = 0.6;
-    private double power = 0;
-    private final ElapsedTime timer = new ElapsedTime();
-    public void init(HardwareMap hwMap){
-        turret = hwMap.get(DcMotorEx.class, "turret");
-        turret.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-    }
-    public void setKp(double newKp){
-        kP = newKp;
-    }
-    public double getKp(){
-        return kP;
-    }
-    public void setKd(double newKd){
-        kD = newKd;
-    }
-    public double getKd(){
-        return kD;
-    }
-    public void resetTimer(){
-        timer.reset();
-    }
-    public void update(AprilTagDetection currId){
-        double deltaTime = timer.seconds();
-        timer.reset();
-
-        if(currId == null){
-            turret.setPower(0);
-            lastError = 0;
-            return;
-        }
-        double error = goalX - currId.ftcPose.bearing;
-        double pTerm = error * kP;
-        double dTerm = 0;
-        if (deltaTime > 0 ){
-            dTerm = (error - lastError)/deltaTime*kD;
-        }
-        if (Math.abs(error) < angleTolerance){
-            power = 0;
-        }
-        else{
-            power = Range.clip(pTerm + dTerm, -MAX_POWER, MAX_POWER);
-        }
-
-        turret.setPower(power);
-        lastError = error;
-    }
-}
+//package org.firstinspires.ftc.teamcode;
+//
+//import com.qualcomm.robotcore.hardware.DcMotor;
+//import com.qualcomm.robotcore.hardware.DcMotorEx;
+//import com.qualcomm.robotcore.hardware.HardwareMap;
+//import com.qualcomm.robotcore.util.ElapsedTime;
+//import com.qualcomm.robotcore.util.Range;
+//
+//import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+//
+//public class turretMechanism {
+//    private DcMotorEx turret;
+//    private double kP = 0.0001;
+//    private double kD = 0.000;
+//    private double goalX = 0;
+//    private double lastError = 0;
+//    private double angleTolerance = 0.2;
+//    private final double MAX_POWER = 0.6;
+//    private double power = 0;
+//    private final ElapsedTime timer = new ElapsedTime();
+//    public void init(HardwareMap hwMap){
+//        turret = hwMap.get(DcMotorEx.class, "turret");
+//        turret.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//    }
+//    public void setKp(double newKp){
+//        kP = newKp;
+//    }
+//    public double getKp(){
+//        return kP;
+//    }
+//    public void setKd(double newKd){
+//        kD = newKd;
+//    }
+//    public double getKd(){
+//        return kD;
+//    }
+//    public void resetTimer(){
+//        timer.reset();
+//    }
+//    public void update(AprilTagDetection currId){
+//        double deltaTime = timer.seconds();
+//        timer.reset();
+//
+//        if(currId == null){
+//            turret.setPower(0);
+//            lastError = 0;
+//            return;
+//        }
+//        double error = goalX - currId.ftcPose.bearing;
+//        double pTerm = error * kP;
+//        double dTerm = 0;
+//        if (deltaTime > 0 ){
+//            dTerm = (error - lastError)/deltaTime*kD;
+//        }
+//        if (Math.abs(error) < angleTolerance){
+//            power = 0;
+//        }
+//        else{
+//            power = Range.clip(pTerm + dTerm, -MAX_POWER, MAX_POWER);
+//        }
+//
+//        turret.setPower(power);
+//        lastError = error;
+//    }
+//}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/turretOpModeAlign.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/turretOpModeAlign.java
@@ -1,55 +1,55 @@
-package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
-import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
-
-public class turretOpModeAlign extends OpMode {
-    private AprilTagWebcam  aprilTagWebcam = new AprilTagWebcam();
-    private turretMechanism turret = new turretMechanism();
-    double[] stepSizes = {0.1, 0.01, 0.001, 0.0001};
-    int stepIndex = 2;
-    @Override
-    public void init(){
-        aprilTagWebcam.init(hardwareMap, telemetry);
-        turret.init(hardwareMap);
-        telemetry.addLine("Initialized All mechanisms");
-    }
-    public void start(){
-        turret.resetTimer();
-    }
-    @Override
-    public void loop(){
-        aprilTagWebcam.update();
-        AprilTagDetection id20 = aprilTagWebcam.getTagBySpecificIds(20);
-        turret.update(id20);
-
-        if(gamepad1.bWasPressed()){
-            stepIndex = (stepIndex + 1) % stepSizes.length;
-        }
-        if(gamepad1.dpadLeftWasPressed()){
-            stepIndex = turret.setKp(turret.getKp() - stepSizes[stepIndex]);
-        }
-        if(gamepad1.dpadRightWasPressed()){
-            stepIndex = turret.setKp(turret.getKp() + stepSizes[ stepIndex]);
-        }
-        if(gamepad1.dpadDownWasPressed()){
-            stepIndex = turret.setKd(turret.getKd() - stepSizes[ stepIndex]);
-        }
-        if(gamepad1.dpadUpWasPressed()){
-            stepIndex = turret.setKd(turret.getKd() + stepSizes[ stepIndex]);
-        }
-        if(id20 != null){
-            telemetry.addData("currId: ", aprilTagWebcam);
-        }
-        else{
-            telemetry.addLine("No tag detected");
-        }
-        telemetry.addLine("--------------------------------");
-        telemetry.addData("Tuning P", "%.5f D-Pad Up/Down", turret.getKp());
-        telemetry.addData("Tuning D", "%.5f D-Pad Left/Right", turret.getKd());
-        telemetry.addData("Step Size", "%.5f B button", stepSizes[(int)stepIndex]);
-    }
-
-}
+//package org.firstinspires.ftc.teamcode;
+//
+//import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+//import com.qualcomm.robotcore.hardware.HardwareMap;
+//
+//import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+//
+//public class turretOpModeAlign extends OpMode {
+//    private AprilTagWebcam  aprilTagWebcam = new AprilTagWebcam();
+//    private turretMechanism turret = new turretMechanism();
+//    double[] stepSizes = {0.1, 0.01, 0.001, 0.0001};
+//    int stepIndex = 2;
+//    @Override
+//    public void init(){
+//        aprilTagWebcam.init(hardwareMap, telemetry);
+//        turret.init(hardwareMap);
+//        telemetry.addLine("Initialized All mechanisms");
+//    }
+//    public void start(){
+//        turret.resetTimer();
+//    }
+//    @Override
+//    public void loop(){
+//        aprilTagWebcam.update();
+//        AprilTagDetection id20 = aprilTagWebcam.getTagBySpecificIds(20);
+//        turret.update(id20);
+//
+//        if(gamepad1.bWasPressed()){
+//            stepIndex = (stepIndex + 1) % stepSizes.length;
+//        }
+//        if(gamepad1.dpadLeftWasPressed()){
+//            stepIndex = turret.setKp(turret.getKp() - stepSizes[stepIndex]);
+//        }
+//        if(gamepad1.dpadRightWasPressed()){
+//            stepIndex = turret.setKp(turret.getKp() + stepSizes[ stepIndex]);
+//        }
+//        if(gamepad1.dpadDownWasPressed()){
+//            stepIndex = turret.setKd(turret.getKd() - stepSizes[ stepIndex]);
+//        }
+//        if(gamepad1.dpadUpWasPressed()){
+//            stepIndex = turret.setKd(turret.getKd() + stepSizes[ stepIndex]);
+//        }
+//        if(id20 != null){
+//            telemetry.addData("currId: ", aprilTagWebcam);
+//        }
+//        else{
+//            telemetry.addLine("No tag detected");
+//        }
+//        telemetry.addLine("--------------------------------");
+//        telemetry.addData("Tuning P", "%.5f D-Pad Up/Down", turret.getKp());
+//        telemetry.addData("Tuning D", "%.5f D-Pad Left/Right", turret.getKd());
+//        telemetry.addData("Step Size", "%.5f B button", stepSizes[(int)stepIndex]);
+//    }
+//
+//}
